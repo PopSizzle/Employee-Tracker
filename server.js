77db108ee646769307663f8c276dd8f7fb10b130
Sam Poppe <poppe.samuel@gmail.com>
2020-04-21 09:39:22 -0700

    constructing console table

--- server.js
+++ server.js
@@ -1,3 +1,34 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 const ctable = require("console.table");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    port: 3306,
+
+    user: "root",
+
+    password: "password",
+    database: "employee_trackerDB"
+});
+
+connection.connect(function(err) {
+    if(err) throw err;
+    generateTable();
+});
+
+function generateTable(){
+    const query = "SELECT * from employees";
+    connection.query(query, function(err, res) {
+        const values = [];
+        for(i=0; i<res.length; i++){
+            let obj = [];
+            obj.push(res[i].first_name);
+            obj.push(res[i].last_name);
+            values.push(obj);
+        }
+
+        console.table(["First Name", "Last Name"], values);
+    });
+}
